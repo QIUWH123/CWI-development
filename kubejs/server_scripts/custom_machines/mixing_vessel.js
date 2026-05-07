94a434a9430ef8e2fafa271c9284c7dfc28ec6fe
QIUWH <1537326617@qq.com>
2026-05-07 23:48:40 +0800

    CWI Beta 0.3.37

--- kubejs/server_scripts/custom_machines/mixing_vessel.js
+++ kubejs/server_scripts/custom_machines/mixing_vessel.js
@@ -1,33 +1,10 @@
-(() => {
-    const MACHINE_ID = 'modpack:mixing_vessel';
-    const REQUIRED_TAG = 'kubejs:mixing_vessel';
-    function checkFloor(machine) {
-        return machine.getLevel()
-            .getBlock(machine.getPos().offset(0, -1, 0))
-            .hasTag(REQUIRED_TAG);
+MBDMachineEvents.onTick('modpack:mixing_vessel', event => {
+    const machine = event.getEvent().getMachine()
+    const isValid = machine.getLevel()
+        .getBlock(machine.getPos().offset(0, -1, 0))
+        .hasTag('kubejs:mixing_vessel')
+    if (machine.recipeLogic && machine.recipeLogic.setWorkingEnabled) {
+        machine.recipeLogic.setWorkingEnabled(isValid)
     }
-    function updateMachineState(machine) {
-        const isValid = checkFloor(machine);
-        if (machine.recipeLogic && machine.recipeLogic.setWorkingEnabled) {
-            machine.recipeLogic.setWorkingEnabled(isValid);
-        }
-        machine.getCustomData().putBoolean('hasValidFloor', isValid);
-    }
-    MBDMachineEvents.onPlaced(MACHINE_ID, event => {
-        const machine = event.getEvent().getMachine();
-        updateMachineState(machine);
-    });
-    const lastFloorState = new Map();
-    MBDMachineEvents.onTick(MACHINE_ID, event => {
-        const machine = event.getEvent().getMachine();
-        const isValid = checkFloor(machine);
-        const posKey = machine.getPos().asLong();
-        if (lastFloorState.get(posKey) !== isValid) {
-            lastFloorState.set(posKey, isValid);
-            machine.getCustomData().putBoolean('hasValidFloor', isValid);
-            if (machine.recipeLogic && machine.recipeLogic.setWorkingEnabled) {
-                machine.recipeLogic.setWorkingEnabled(isValid);
-            }
-        }
-    });
-})();
+    machine.getCustomData().putBoolean('hasValidFloor', isValid)
+})
