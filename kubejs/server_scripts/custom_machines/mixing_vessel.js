daa3cc7f97c51476b83ed72315d1992988359057
QIUWH <1537326617@qq.com>
2026-05-07 23:59:16 +0800

    CWI Beta 0.3.37

--- kubejs/server_scripts/custom_machines/mixing_vessel.js
+++ kubejs/server_scripts/custom_machines/mixing_vessel.js
@@ -3,8 +3,7 @@ MBDMachineEvents.onTick('modpack:mixing_vessel', event => {
     const isValid = machine.getLevel()
         .getBlock(machine.getPos().offset(0, -1, 0))
         .hasTag('kubejs:mixing_vessel')
-    if (machine.recipeLogic && machine.recipeLogic.setWorkingEnabled) {
+    if (machine.recipeLogic) {
         machine.recipeLogic.setWorkingEnabled(isValid)
     }
-    machine.getCustomData().putBoolean('hasValidFloor', isValid)
 })
