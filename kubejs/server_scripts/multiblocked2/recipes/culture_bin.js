1c7c33b39d0cd8183e28c27e93713ce978ff4608
QIUWH <1537326617@qq.com>
2026-05-17 00:50:42 +0800

    CWi Beta 0.3.37

--- kubejs/server_scripts/multiblocked2/recipes/culture_bin.js
+++ kubejs/server_scripts/multiblocked2/recipes/culture_bin.js
@@ -5,6 +5,6 @@ ServerEvents.recipes(event => {
     .priority(0).duration(100)
     .inputItems("tfmg:nitrate_dust")
     .inputFluids("kubejs:distilled_water 125")
-    .outputFluids("kubejs:nitrate_solution 125");
+    .outputFluids("kubejs:nitrate_solution 125")
 
-});
+})
