29e3117ebc164023d100e33e5913968ae0a0671f
QIUWH <1537326617@qq.com>
2026-06-11 16:08:30 +0800

    CWI Beta 0.3.39

--- kubejs/server_scripts/Recipes/BulkProcessing.js
+++
@@ -1,7 +0,0 @@
-
-// Corroding Recipe
-global.corrodingFanRecipes.put('create:iron_sheet', 'kubejs:iron_dust')
-global.corrodingFanRecipes.put('minecraft:iron_ingot', 'kubejs:iron_dust')
-
-// Sterilizing Recipe
-global.sterilizingFanRecipes.put('kubejs:petri_dish', 'kubejs:sterile_petri_dish')
