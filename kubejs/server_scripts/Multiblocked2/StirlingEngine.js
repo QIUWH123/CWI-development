35d87bd76da9963ff897c7c7539965b3b556c500
QIUWH <1537326617@qq.com>
2026-07-10 13:32:12 -0700

    CWI Beta 0.3.39

--- kubejs/server_scripts/Multiblocked2/StirlingEngine.js
+++ kubejs/server_scripts/Multiblocked2/StirlingEngine.js
@@ -1,89 +1,71 @@
 const $IMachine = Java.loadClass('com.lowdragmc.mbd2.api.machine.IMachine')
 const $BlazeBurnerBlock = Java.loadClass('com.simibubi.create.content.processing.burner.BlazeBurnerBlock')
 
-MBDMachineEvents.onTick('cwi:stirling_engine', event => {
+const directionOffsets = {}
+directionOffsets[Direction.NORTH] = [-0.1, 0.4, 1.1, 0.8, -0.1, 0.8, 1.1, 0.4]
+directionOffsets[Direction.EAST]  = [0.6, -0.1, 0.2, 1.1, 0.2, -0.1, 0.6, 1.1]
+directionOffsets[Direction.SOUTH] = [-0.1, 0.2, 1.1, 0.6, -0.1, 0.6, 1.1, 0.2]
+directionOffsets[Direction.WEST]  = [0.8, -0.1, 0.4, 1.1, 0.4, -0.1, 0.8, 1.1]
 
-    if (globalTickCounter % 20 !== 0) return
-
-    const machine = event.getEvent().getMachine()
+function updateMachine(machine) {
     const level = machine.getLevel()
     const pos = machine.getPos()
-    
-    const blockBelow = level.getBlockState(pos.offset(0, -1, 0))
-    let heatLevel = null
+    const blockBelowPos = pos.offset(0, -1, 0)
+    const blockBelowState = level.getBlockState(blockBelowPos)
     let targetRPM = 0
 
-    if (level.getBlock(pos.offset(0, -1, 0)).hasTag('create:passive_boiler_heaters')){
+    if (level.getBlock(blockBelowPos).hasTag('create:passive_boiler_heaters')) {
         targetRPM = 32
     }
-
-    if (blockBelow.hasProperty($BlazeBurnerBlock.HEAT_LEVEL)) {
-        heatLevel = blockBelow.getValue($BlazeBurnerBlock.HEAT_LEVEL)
-        switch (heatLevel.name()) {
-            case 'KINDLED':
-                targetRPM = 64
-                break
-            case 'SEETHING':
-                targetRPM = 128
-                break
-        }
+    if (blockBelowState.hasProperty($BlazeBurnerBlock.HEAT_LEVEL)) {
+        const heatName = blockBelowState.getValue($BlazeBurnerBlock.HEAT_LEVEL).name()
+        if (heatName === 'KINDLED') targetRPM = 64
+        else if (heatName === 'SEETHING') targetRPM = 128
     }
 
-    const working = targetRPM !== 0
+    const isWorking = targetRPM !== 0
+    machine.recipeLogic.setWorkingEnabled(isWorking)
 
-    if (working) {
-        level.playSound(null, pos.x + 0.5, pos.y + 0.5, pos.z + 0.5, 'create:steam', 'blocks', 0.4, 1)
-        
-        let facing = machine.getFrontFacing().get()
-        let x1, z1, x2, z2, x3, z3, x4, z4
-        const yBase = pos.y + 0.6
+    const machineHolder = $IMachine.ofMachine(level, pos).orElse(null).machineHolder
+    if (machine.customData.getInt('rpm') === targetRPM) return
 
-        if (facing == Direction.NORTH) {
-            x1 = pos.x - 0.1; z1 = pos.z + 0.4
-            x2 = pos.x + 1.1; z2 = pos.z + 0.8
-            x3 = pos.x - 0.1; z3 = pos.z + 0.8
-            x4 = pos.x + 1.1; z4 = pos.z + 0.4
-        } else if (facing == Direction.EAST) {
-            x1 = pos.x + 0.6; z1 = pos.z - 0.1
-            x2 = pos.x + 0.2; z2 = pos.z + 1.1
-            x3 = pos.x + 0.2; z3 = pos.z - 0.1
-            x4 = pos.x + 0.6; z4 = pos.z + 1.1
-        } else if (facing == Direction.SOUTH) {
-            x1 = pos.x - 0.1; z1 = pos.z + 0.2
-            x2 = pos.x + 1.1; z2 = pos.z + 0.6
-            x3 = pos.x - 0.1; z3 = pos.z + 0.6
-            x4 = pos.x + 1.1; z4 = pos.z + 0.2
-        } else if (facing == Direction.WEST) {
-            x1 = pos.x + 0.8; z1 = pos.z - 0.1
-            x2 = pos.x + 0.4; z2 = pos.z + 1.1
-            x3 = pos.x + 0.4; z3 = pos.z - 0.1
-            x4 = pos.x + 0.8; z4 = pos.z + 1.1
-        }
+    isWorking ? machineHolder.scheduleWorkingRPM(targetRPM, false) : machineHolder.stopWorking()
 
-        if (globalTickCounter % 40 !== 0) {
-            level.spawnParticles('create_sa:steam_cloud', true, x1, yBase, z1, 0, 0, 0, 0, 0)
-            level.spawnParticles('create_sa:steam_cloud', true, x2, yBase, z2, 0, 0, 0, 0, 0)
-        } else {
-            level.spawnParticles('create_sa:steam_cloud', true, x3, yBase, z3, 0, 0, 0, 0, 0)
-            level.spawnParticles('create_sa:steam_cloud', true, x4, yBase, z4, 0, 0, 0, 0, 0)
-        }
-    }
+    machine.customData.putBoolean('work', isWorking)
+    machine.customData.putInt('rpm', targetRPM)
+}
 
-    const partmachine = $IMachine.ofMachine(level, pos).orElse(null)
-    if (!partmachine) return
+MBDMachineEvents.onPlaced('cwi:stirling_engine', event => {
+    const machine = event.getEvent().getMachine()
+    machine.getLevel().getServer().scheduleInTicks(1, () => {
+        updateMachine(machine)
 
-    const holder = partmachine.machineHolder
-    if (!holder) return
+        const pos = machine.getPos()
+        const offsets = directionOffsets[machine.getFrontFacing().get()]
+        const particleY = pos.y + 0.6
+        const particleCoords = offsets.map((val, idx) => idx % 2 === 0 ? pos.x + val : pos.z + val)
+        machine.customData.putString('pts', `${particleY},${particleCoords.join(',')}`)
+    })
+})
 
-    const prevValid = machine.customData.getInt('stirling_rpm')
-    if (prevValid == targetRPM) return
+MBDMachineEvents.onNeighborChanged('cwi:stirling_engine', event => {
+    updateMachine(event.getEvent().getMachine())
+})
 
-    if (working) {
-        holder.scheduleWorkingRPM(targetRPM, false)
-    } else {
-        holder.stopWorking()
-    }
+MBDMachineEvents.onTick('cwi:stirling_engine', event => {
+    if (globalTickCounter % 20) return
 
-    machine.recipeLogic.setWorkingEnabled(working)
-    machine.customData.putInt('stirling_rpm', targetRPM)
+    const machine = event.getEvent().getMachine()
+    if (!machine.customData.getBoolean('work')) return
+
+    const level = machine.getLevel()
+    const pos = machine.getPos()
+    level.playSound(null, pos.x + 0.5, pos.y + 0.5, pos.z + 0.5, 'create:steam', 'blocks', 0.4, 1)
+
+    const particleData = machine.customData.getString('pts').split(',').map(Number)
+    const [y, x1, z1, x2, z2, x3, z3, x4, z4] = particleData
+    const useAlt = globalTickCounter % 40 !== 0
+
+    level.spawnParticles('create_sa:steam_cloud', true, useAlt ? x1 : x3, y, useAlt ? z1 : z3, 0, 0, 0, 0, 0)
+    level.spawnParticles('create_sa:steam_cloud', true, useAlt ? x2 : x4, y, useAlt ? z2 : z4, 0, 0, 0, 0, 0)
 })
